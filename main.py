80f94fdb5f1e1db099eb379bb8fc69ea47086391
Daniel Humer <danielhumer.at@icloud.com>
2020-09-12 18:44:42 +0200

    finished

---
+++ main.py
@@ -0,0 +1,14 @@
+mood = input("What's your mood? ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Come on smile a little bit.")
+elif mood == "excited":
+    print("You are so excited.")
+elif mood == "relaxed":
+    print("Let`s chill on the couch!")
+else:
+    print("I don't recognize this mood.")
